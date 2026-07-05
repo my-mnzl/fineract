85df48e1229a727767772f29b621d41de36309b6
Austin Matsick <austin@mnzl.co>
2026-07-05 17:54:15 +0200

    [codex] Widen job run history error log (#29)
    * Widen job run history error log
    
    * Widen job run history error message

--- fineract-provider/src/main/resources/sql/migrations/sample_data/barebones_db.sql
+++ fineract-provider/src/main/resources/sql/migrations/sample_data/barebones_db.sql
@@ -413,9 +413,9 @@ CREATE TABLE IF NOT EXISTS `job_run_history` (
   `start_time` datetime NOT NULL,
   `end_time` datetime NOT NULL,
   `status` varchar(10) CHARACTER SET latin1 NOT NULL,
-  `error_message` text,
+  `error_message` longtext,
   `trigger_type` varchar(25) NOT NULL,
-  `error_log` text,
+  `error_log` longtext,
   PRIMARY KEY (`id`),
   KEY `scheduledjobsFK` (`job_id`),
   CONSTRAINT `scheduledjobsFK` FOREIGN KEY (`job_id`) REFERENCES `job` (`id`)
